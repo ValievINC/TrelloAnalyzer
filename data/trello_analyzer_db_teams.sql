755e911e21cf178a61347bf816e7ef282ee79e9f
NikitaSamkov <nick.samkov@yandex.ru>
2021-12-12 19:04:10 +0500

    db update

--- data/trello_analyzer_db_teams.sql
+++ data/trello_analyzer_db_teams.sql
@@ -7,5 +7,6 @@ create table teams
     url   varchar(255) not null
 );
 
-INSERT INTO trello_analyzer_db.teams (id, name, topic, url) VALUES (3, 'test1', 'djfhuodihfdjfodj', 'suidfhsidh.com');
-INSERT INTO trello_analyzer_db.teams (id, name, topic, url) VALUES (4, 'Nibble Studio', 'Trello Anal yzer', 'нету пока :(');
+INSERT INTO trello_analyzer_db.teams (id, name, topic, url) VALUES (7, 'Calculus Bakery', 'Игра по математике', 'https://trello.com/b/WgYpbucY/calculus-bakery');
+INSERT INTO trello_analyzer_db.teams (id, name, topic, url) VALUES (8, 'TrelloAnalyzer', 'Создать анализатор досок Trello', 'https://trello.com/b/XllYwyv4/%D1%82%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%B0%D1%8F-%D0%B4%D0%BE%D1%81%D0%BA%D0%B0');
+INSERT INTO trello_analyzer_db.teams (id, name, topic, url) VALUES (9, 'NS Team', '2д рогалик на C#', 'https://trello.com/b/jzAF2ZN0/great-hero');
--- data/trello_analyzer_db_users.sql
+++ data/trello_analyzer_db_users.sql
@@ -10,11 +10,7 @@ create table users
         foreign key (team_id) references teams (id)
 );
 
-INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (8, 'Nikita', 'Samkov', 3, 'leader');
-INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (9, 'Никита', 'Самков', 3, 'помощник');
-INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (10, 'N', 'S', 3, 'NS');
-INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (11, 'Булат', 'Валиев', 4, 'Тимлид');
-INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (12, 'Никита', 'Самков', 4, 'Бэкэнд');
-INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (13, 'Эммануил', 'Сафоян', 4, 'Бэкэнд');
-INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (14, 'Любовь', 'Лила', 4, 'Прогер');
-INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (15, 'Никита', 'Югай', 4, 'Аналитик');
+INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (8, 'Юрий', 'Перов', 7, 'Бэкэнд');
+INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (9, 'Булат', 'Валиев', 8, 'Тимлид');
+INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (19, 'Никита', 'Самков', 8, 'Бэкэнд');
+INSERT INTO trello_analyzer_db.users (id, first_name, last_name, team_id, role) VALUES (20, 'Никита', 'Самков', 9, 'Тимлид');
