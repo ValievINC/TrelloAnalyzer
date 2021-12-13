557f20dc3f50360b7d997e69638769c03295dd12
NikitaSamkov <nick.samkov@yandex.ru>
2021-12-13 20:01:19 +0500

    search fix

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ $db = new DBConnection('trelloanalyzer', 'trello_analyzer_db');
 
 $teams = $db->GetColumns('teams', ['id', 'team_name']);
 
+print_r($_POST);
+
 $team_info = (array_key_exists('team_id', $_POST)) ? $db->GetRow('teams', 'id', $_POST['team_id'])[0] :
     ['id' => -1, 'team_name' => 'Команда не найдена :(', 'topic' => 'Возможно, допущена ошибка в написании названия команды', 'url' => ''];
 $members = (array_key_exists('team_id', $_POST)) ? $db->GetRow('users', 'team_id', $_POST['team_id']) : [];
