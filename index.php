87e008f3d0e0dcb9e641394efba9a978b9b15a4d
NikitaSamkov <nick.samkov@yandex.ru>
2021-12-23 13:42:50 +0500

    removed print_r($_POST)

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ $db = new DBConnection('trelloanalyzer', 'trello_analyzer_db');
 
 $teams = $db->GetColumns('teams', ['id', 'team_name']);
 
-print_r($_POST);
-
 $tn = (array_key_exists('team_name', $_POST)) ? $_POST['team_name'] : '';
 
 $team_info = $tn ? $db->GetRow('teams', 'team_name', "'".$tn."'")[0] :
